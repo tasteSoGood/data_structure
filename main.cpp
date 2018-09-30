5b5c78fca1991ba23d646eab2d6eb2cce18815ce
tasteSoGood <970879461@qq.com>
2018-09-30 12:22:27 +0800

    git add .

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
  * 本脚本用于算法的测试
  */
 #include <iostream>
-#include "vector/vector.hpp"
+#include "resource/vector.hpp"
 
 using namespace std;
 int main() {
     int a[] = {1, 2, 3, 4, 5};
     vector<int> test(a, 0, 5);
+    cout << test[13] << endl;
     return 0;
 }
---
+++ resource/error.hpp
@@ -0,0 +1,7 @@
+/*
+ * 定义多种异常类
+ */
+#include <exception>
+
+class out_bound_error : public std::exception {
+};
